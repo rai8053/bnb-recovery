ba5ebbcacbd6e19fc9a61698ca2a1e5c643e9d13
rai8053 <54878934+rai8053@users.noreply.github.com>
2025-05-15 00:22:15 +0530

    Update recover_token.py

--- recover_token.py
+++ recover_token.py
@@ -5,10 +5,10 @@ import json
 w3 = Web3(Web3.HTTPProvider("https://bsc-dataseed.binance.org/"))
 
 # Step 2: Wallet and Contract Info
-private_key = "274320f79bb53daf6ee6f1dbf854c21e648c4ee70cebfb68bf43f73bb8306329"
-from_address = w3.to_checksum_address("0x60eBf48C80c6b540F712F2aF4Aad0175ED5Ab08d")
-to_address = w3.to_checksum_address("0x1b713b14cd3a813366524a1e82864bd064d839a6")  # CEX deposit address
-token_address = w3.to_checksum_address("0x899357E54C2c4b014ea50A9A7Bf140bA6Df2eC73")  # Token contract
+private_key = "your privet key"
+from_address = w3.to_checksum_address("your compromise wallet address")
+to_address = w3.to_checksum_address("your deposit address")  # CEX deposit address
+token_address = w3.to_checksum_address("your token contract address")  # Token contract
 
 # Step 3: Minimal ERC-20 ABI
 token_abi = json.loads('[{"name":"transfer","type":"function","inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"outputs":[{"name":"","type":"bool"}],"stateMutability":"nonpayable"},{"name":"balanceOf","type":"function","inputs":[{"name":"_owner","type":"address"}],"outputs":[{"name":"balance","type":"uint256"}],"stateMutability":"view"}]')
